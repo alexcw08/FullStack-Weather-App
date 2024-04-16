5586e7727c1ccaeede6cb4eb41208146d637938d
Alex C <calex781@gmail.com>
2024-04-16 17:16:29 -0400

    Catch errors with Geocode API. Remove redundant try/catch block.
    - Geocode API responds with status string to indicate errors. Added if statements to threw new errors.
    - Try catch block was redundant because error was not being modified, error would be automatically propagated to caller.

--- backend/src/services/locService.js
+++ backend/src/services/locService.js
@@ -3,13 +3,19 @@ import axios from "axios";
 const fetchLocation = async (address) => {
   // Takes in address - returns object containing lat and long
   const key = process.env.GEOCODE_KEY;
-  try {
-    const geoRes = await axios.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`
-    );
+  const geoRes = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`
+  );
+
+  // check for errors with query
+  if (geoRes.data.status === "OK") {
     return geoRes.data.results[0].geometry.location;
-  } catch (error) {
-    throw error;
+  } else if (geoRes.data.status === "ZERO_RESULTS") {
+    throw new Error("Zero results for query.");
+  } else if (geoRes.data.status === "INVALID_REQUEST") {
+    throw new Error("Invalid query.");
+  } else {
+    throw new Error("Geocode API error.");
   }
 };
 
