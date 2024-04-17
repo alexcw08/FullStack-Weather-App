7432d753549d800e469480b18af8b566a884d1d6
Alex C <calex781@gmail.com>
2024-04-17 10:13:16 -0400

    Install express-validator, implement query validation.
    - Use validation chain to trim white space, check if empty, and sanitize to prevent XSS vulnerabilities.
    - If no errors, proceed to try/catch block, else, respond to client with 400 err.

--- backend/src/routes/weatherRoutes.js
+++ backend/src/routes/weatherRoutes.js
@@ -1,24 +1,34 @@
 import fetchLocation from "../services/locService.js";
 import fetchWeather from "../services/weatherService.js";
+import { query, validationResult } from "express-validator";
 
 const weatherRoutes = (app) => {
-  app.get("/weather", async (req, res) => {
-    // receives location for weather
-    const location = req.query.location;
-    // TODO: IMPLEMENT INPUT VALIDATION
-    try {
-      // fetch coordinates for the location requested
-      const coords = await fetchLocation(location);
-      // fetch weather for coordinates
-      const weatherData = await fetchWeather(coords.lat, coords.lng);
-      // send data back to client
-      res.send(weatherData);
-    } catch (error) {
-      // TODO: LOOK INTO IMPROVED LOGGING SOLUTIONS
-      console.log(error);
-      res.status(500).json({ error: "Server error." });
+  app.get(
+    "/weather",
+    query("location").trim().notEmpty().escape(),
+    async (req, res) => {
+      // validate location
+      const validation = validationResult(req);
+      if (validation.isEmpty()) {
+        // receives location for weather
+        const location = req.query.location;
+        try {
+          // fetch coordinates for the location requested
+          const coords = await fetchLocation(location);
+          // fetch weather for coordinates
+          const weatherData = await fetchWeather(coords.lat, coords.lng);
+          // send data back to client
+          res.send(weatherData);
+        } catch (error) {
+          res.status(500).json({ error: error.message });
+        }
+      } else {
+        res
+          .status(400)
+          .json({ error: "Invalid input. Location must not be empty." });
+      }
     }
-  });
+  );
 };
 
 export default weatherRoutes;
