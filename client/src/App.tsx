44f15ae78b7e7a98bd6da92e0e7fb6996e1b2c54
Alex C <calex781@gmail.com>
2024-04-20 20:46:39 -0400

    Install react icons, create a header component for searchbar, favorites, and settings.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Header from "./components/Header";
 function App() {
-  return <div className="bg-[#3B4664] h-dvh w-full}"></div>;
+  return (
+    <div className="bg-[#3B4664] px-[5%] h-dvh w-full}">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
