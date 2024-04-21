b94e7a5f0522ba9d57d611bc1a2cbd8f44afd593
Alex C <calex781@gmail.com>
2024-04-20 21:40:43 -0400

    Add component for hourly temperatures.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
+import HourlyCast from "./components/HourlyCast";
 function App() {
   return (
-    <div className="bg-[#3B4664] px-[5%] h-dvh w-full}">
+    <div className="bg-[#3B4664] px-[5%] flex flex-col gap-2 h-dvh w-full}">
       <Header />
+      <HourlyCast />
     </div>
   );
 }
