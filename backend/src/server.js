6bccc7b60ed4b54347a0cd3e84645001f4a844c4
Alex C <calex781@gmail.com>
2024-04-22 13:34:04 -0400

    Install cors middleware.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import weatherRoutes from "./routes/weatherRoutes.js";
 import dotenv from "dotenv";
-
+import cors from "cors";
 // set up
 const app = express();
 const PORT = 3001;
 dotenv.config();
 // mount json middleware
 app.use(express.json());
+// mount cors middleware
+app.use(cors());
 
 weatherRoutes(app);
 // start server
