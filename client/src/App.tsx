5caaa85ee2843036cdd121d66ba2398581ae4ad9
Alex C <calex781@gmail.com>
2024-04-22 13:10:41 -0400

    Remove old unneeded components.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import Header from "./components/Header";
-import HourlyCast from "./components/HourlyCast";
+import CurrentCast from "./components/CurrentCast";
+import HourlyDaily from "./components/HourlyDaily";
+
 function App() {
   return (
-    <div className="bg-[#3B4664] px-[5%] flex flex-col gap-2 h-dvh w-full}">
-      <Header />
-      <HourlyCast />
+    <div className="bg-[#D1D8DC] flex h-dvh w-full}">
+      <CurrentCast />
+      <HourlyDaily />
     </div>
   );
 }
