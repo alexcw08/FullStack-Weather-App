ae4e2f5e2f0ab425e6b688a73ab7884c7e5e49f2
Alex C <calex781@gmail.com>
2024-04-22 13:44:17 -0400

    Make a get request to our backend with location to receive weather data.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import CurrentCast from "./components/CurrentCast";
 import HourlyDaily from "./components/HourlyDaily";
 
 function App() {
+  const [location, setLocation] = useState("");
   return (
     <div className="bg-[#D1D8DC] flex h-dvh w-full}">
       <CurrentCast />
-      <HourlyDaily />
+      <HourlyDaily location={location} setLocation={setLocation} />
     </div>
   );
 }
--- client/src/components/HourlyDaily.tsx
+++ client/src/components/HourlyDaily.tsx
@@ -1,9 +1,28 @@
-export default function HourlyDaily() {
+import axios from "axios";
+
+export default function HourlyDaily({ location, setLocation }) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.get(
+      `http://localhost:3001/weather?location=${location}`
+    );
+    console.log(res);
+  };
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  };
+
   return (
     <div className=" w-[60%] px-20 flex flex-col">
-      <form action="get" className="w-1/2 mx-auto">
-        <label className="input input-bordered  flex items-center justify-between  bg-[#BFC5C9] ">
+      <form onSubmit={handleSubmit} className="w-1/2 mx-auto">
+        <label
+          htmlFor="location"
+          className="input input-bordered  flex items-center justify-between  bg-[#BFC5C9] "
+        >
           <input
+            name="location"
+            value={location}
+            onChange={handleChange}
             className="placeholder-[#494949] text-[#1F3753]"
             placeholder="Search for location"
           />
@@ -15,9 +34,9 @@ export default function HourlyDaily() {
               className="w-6 h-6 opacity-100"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
           </button>
