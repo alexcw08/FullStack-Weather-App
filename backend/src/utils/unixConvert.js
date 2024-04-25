2d6d1b461f3fa908c3f2f4b53b1d8f7e04a36adf
Alex C <calex781@gmail.com>
2024-04-25 08:21:02 -0400

    Implement converting unix timestamp for the sunset and sunrise.

--- backend/src/utils/unixConvert.js
+++ backend/src/utils/unixConvert.js
@@ -1,8 +1,10 @@
 import getDayAndTime from "./dateTime.js";
 
 const convertTimeStamps = async (data) => {
-  // convert current data
+  // convert current data time and sunrise/sunset times
   data.current["dt"] = await getDayAndTime(data.current["dt"]);
+  data.current["sunrise"] = await getDayAndTime(data.current["sunrise"]);
+  data.current["sunset"] = await getDayAndTime(data.current["sunset"]);
   // convert hourly data - loop through each hour
   for (let i = 0; i < data.hourly.length; i++) {
     data.hourly[i]["dt"] = await getDayAndTime(data.hourly[i]["dt"]);
@@ -11,7 +13,7 @@ const convertTimeStamps = async (data) => {
   for (let i = 0; i < data.daily.length; i++) {
     data.daily[i]["dt"] = await getDayAndTime(data.daily[i]["dt"]);
   }
-  // return all data now with dates and times
+  // return all data now with dates and times converted
   return data;
 };
 
