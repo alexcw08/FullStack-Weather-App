d49a8b043a59a8d7406c16ffb829eac4d4315cba
Alex C <calex781@gmail.com>
2024-04-25 18:34:43 -0400

    Remove commented out code.

--- client/src/components/HourlyDaily.tsx
+++ client/src/components/HourlyDaily.tsx
@@ -10,14 +10,12 @@ interface ChildProps {
 
 const HourlyDaily: React.FC<ChildProps> = ({ weatherData, setWeatherData }) => {
   return (
-    // <div className=" w-[70%] md:px-16 flex flex-col py-12 overflow-y-scroll">
     <div className="w-[70%] overflow-y-scroll">
       <Header setWeatherData={setWeatherData} />
       <div className="md:px-16 flex flex-col py-12 ">
         <HourlyContainer weatherData={weatherData} />
         <DailyContainer weatherData={weatherData} />
       </div>
-      {/* <Searchbar setWeatherData={setWeatherData} /> */}
     </div>
   );
 };
